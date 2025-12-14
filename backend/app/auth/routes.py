97ea93c51b28a2287368c9bf03299b60dd2cb640
Stelour <stelourgg@gmail.com>
2025-12-15 04:25:08 +0500

    bug fix

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -144,7 +144,7 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],db: A
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.email}, 
-        expires_delta=access_token_expires
+        excd=access_token_expires
     )
     
     return {"access_token": access_token, "token_type": "bearer"}
