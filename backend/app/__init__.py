b84fbaf879191dc3bf1522ce37677b79dac4b446
Stelour <stelourgg@gmail.com>
2025-12-15 05:25:06 +0500

    added init resume routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from .main.routes import router as base_router
 from .auth.routes import router as auth_router
+from .resumes.routes import router as resumes_router
 
 def create_app():
     app = FastAPI(title="fastapi-resume-app")
     app.include_router(base_router)
     app.include_router(auth_router, prefix="/auth")
+    app.include_router(resumes_router, prefix="/resumes")
 
     return app
 
